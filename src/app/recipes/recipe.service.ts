0869b028552ed997c2d0e6f49f645818913de57d
leoleonard <damian.juzyk@gmail.com>
2019-06-18 22:55:17 +0200

    add sending ingredients from recipes to the shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import {Recipe} from './recipe.model';
-import {EventEmitter} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
@@ -8,14 +11,31 @@ export class RecipeService {
     new Recipe(
       'first recipe',
       'it is the very first one',
-      'https://fitfoodiefinds.com/wp-content/uploads/2015/10/classic-oatmeal.png'),
+      'https://fitfoodiefinds.com/wp-content/uploads/2015/10/classic-oatmeal.png',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Fries', 12),
+        new Ingredient('Mayo', 1),
+      ]
+      ),
     new Recipe(
       'second recipe',
       'it is second recipe',
-      'https://www.seriouseats.com/recipes/images/2015/05/Anova-Steak-Guide-Sous-Vide-Photos15-beauty.jpg'),
+      'https://www.seriouseats.com/recipes/images/2015/05/Anova-Steak-Guide-Sous-Vide-Photos15-beauty.jpg',
+      [
+        new Ingredient('Bread', 2),
+        new Ingredient('Butter', 1),
+      ]
+      ),
   ];
+
+  constructor (private slService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
