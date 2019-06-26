43aab16fb654e0251ec5db4c339b0e22287f4d6f
leoleonard <damian.juzyk@gmail.com>
2019-06-26 13:53:39 +0200

    add setup for the form

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -18,5 +19,8 @@ export class AuthComponent implements OnInit {
   }
 
 
-
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    form.reset();
+  }
 }
